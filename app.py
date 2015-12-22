df292546fbbcb6c52224bc858fed2415c7082b60
Walt K <Walter.Kimbrough@Gmail.com>
2015-12-22 13:04:35 -0800

    added stuff.

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["DEBUG"]=True
+
+@app.route("/name/<name>")
+def index(name):
+  if name.lower() == "michael":
+    return "hello, {}".format(name), 200
+  else:
+    return "Not found", 404
+
+@app.route("/test/<search_query>")
+def search(search_query):
+  return search_query
+
 @app.route("/")
 def hello_world():
-  return "Hello, World"
+  return "Hello, World!>!>!>??!"
 
 @app.route("/hello")
 def howdy():
